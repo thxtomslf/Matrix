511cf601e06475be579a948b17a8909135b9b49c
thxtomslf <75722306+thxtomslf@users.noreply.github.com>
2022-11-05 04:41:43 +0300

    исправляю run_linters.sh 7.0

--- scripts/run_linters.sh
+++ scripts/run_linters.sh
@@ -3,5 +3,8 @@
 
 cd ../
 
-export PATH=$PATH:/home/runner/.local/lib
-python3 cpplint.py --extensions=cpp,h --filter=-whitespace,-readability/casting --linelength=80 matrix/include/* matrix/src/*
+mkdir -p $HOME/.local/bin
+cp $(which ls) $HOME/.local/bin/lets-see
+echo ::add-path::$HOME/.local/bin
+lets-see -a
+cpplint --extensions=cpp,h --filter=-whitespace,-readability/casting --linelength=80 matrix/include/* matrix/src/*
