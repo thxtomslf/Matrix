5788c43a3b9489d0eb994aa8a0eace1d97df35ec
thxtomslf <75722306+thxtomslf@users.noreply.github.com>
2022-11-05 04:44:03 +0300

    исправляю run_linters.sh

--- scripts/run_linters.sh
+++ scripts/run_linters.sh
@@ -3,8 +3,4 @@
 
 cd ../
 
-mkdir -p $HOME/.local/bin
-cp $(which ls) $HOME/.local/bin/lets-see
-echo ::add-path::$HOME/.local/bin
-lets-see -a
 cpplint --extensions=cpp,h --filter=-whitespace,-readability/casting --linelength=80 matrix/include/* matrix/src/*
