6c12b8b062f9fec42a71b4cdccfa002e7e4314bc
thxtomslf <75722306+thxtomslf@users.noreply.github.com>
2022-11-05 04:30:00 +0300

    исправляю run_linters.sh 2.0

--- scripts/run_linters.sh
+++ scripts/run_linters.sh
@@ -2,6 +2,5 @@
 
 
 cd ../
-cd ../../..
-ls -a
-python3 cpplint.py --extensions=cpp,h --filter=-whitespace,-readability/casting --linelength=80 matrix/include/* matrix/src/*
+
+cpplint --extensions=cpp,h --filter=-whitespace,-readability/casting --linelength=80 matrix/include/* matrix/src/*
