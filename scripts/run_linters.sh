432fe9ba8d020747b292c8675d65959add5dfe2f
thxtomslf <75722306+thxtomslf@users.noreply.github.com>
2022-11-08 01:41:40 +0300

    Исправление замечаний статических анализаторов 2.0

--- scripts/run_linters.sh
+++ scripts/run_linters.sh
@@ -4,12 +4,8 @@
 cd ../
 
 echo -e "\n==============cppcheck================"
-cppcheck matrix/include --enable=all --inconclusive --error-exitcode=1 \
+cppcheck matrix --enable=all --inconclusive --error-exitcode=1 \
 -I matrix/include --suppress=missingIncludeSystem
-
-
-echo -e "\n==============clang-tidy================"
-clang-tidy matrix/src/* matrix/include/* -warnings-as-errors=* -extra-arg=-std=c++17 -- -I matrix/include
 
 
 echo -e "\n==============cpplint===================="
